c700b7e835124e40b618705b9c637fa37ec53b01
sj-minRva <rheamaria13@gmail.com>
2025-09-19 23:01:36 +0530

    change port

--- server/index.ts
+++ server/index.ts
@@ -56,16 +56,7 @@ app.use((req, res, next) => {
     serveStatic(app);
   }
 
-  // ALWAYS serve the app on the port specified in the environment variable PORT
-  // Other ports are firewalled. Default to 5000 if not specified.
-  // this serves both the API and the client.
-  // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || '5000', 10);
-  server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
-    log(`serving on port ${port}`);
+  app.listen(5000, "127.0.0.1", () => {
+    console.log("Server running on http://127.0.0.1:5000");
   });
 })();
